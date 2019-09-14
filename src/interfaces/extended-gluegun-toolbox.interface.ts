a165a480134e82c1be6d61f3d3ddf658a3933331
Kai Haase <kai.haase@lenne.tech>
2019-09-14 13:21:07 +0200

    npm extension integrated

--- src/interfaces/extended-gluegun-toolbox.interface.ts
+++ src/interfaces/extended-gluegun-toolbox.interface.ts
@@ -1,9 +1,11 @@
-import { Helper } from '../extensions/helper.extension';
 import { GluegunToolbox } from 'gluegun';
+import { Helper } from '../extensions/helper.extension';
+import { Npm } from '../extensions/npm.extension';
 
 /**
  * Extended GluegunToolbox
  */
 export interface IHelperExtendedGluegunToolbox extends GluegunToolbox {
   helper?: Helper;
+  npm?: Npm;
 }
